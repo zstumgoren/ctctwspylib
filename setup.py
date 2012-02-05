4c9e4632e7171d5e5f9da67a618c27191ab03540
Serdar Tumgoren <tumgorenz@washpost.com>
2012-02-05 14:16:52 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='ctctwspylib',
-      version='0.1.1',
+      version='0.1.1a1',
       description='A clone of the original lib from SourceForge (https://sourceforge.net/projects/ctctwspylib/)',
       author='Serdar Tumgoren',
       author_email='zstumgoren@gmail.com',
@@ -13,7 +13,7 @@ setup(name='ctctwspylib',
       classifiers=[
           'Environment :: Web Environment',
           'Intended Audience :: Developers',
-          'License :: OSI Approved :: Apache 2.0 License',
+          'License :: OSI Approved :: Apache Software License 2.0',
           'Programming Language :: Python',
           'Topic :: Software Development :: Libraries',
       ]
