6663f79193af06d83f9c0fb6e07e521bbc021bb1
Serdar Tumgoren <tumgorenz@washpost.com>
2012-02-05 13:48:40 -0500

    initial setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='ctctwspylib',
+      version='0.1.1',
+      description='A clone of the original lib from SourceForge (https://sourceforge.net/projects/ctctwspylib/)',
+      author='Serdar Tumgoren',
+      author_email='zstumgoren@gmail.com',
+      url='https://github.com/zstumgoren/ctctwspylib',
+      license='Apache 2.0',
+      packages=['ctctwspylib'],
+      install_requires=['csvkit'],
+      classifiers=[
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache 2.0 License',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Libraries',
+      ]
+)
